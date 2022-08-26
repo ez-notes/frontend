4cc2db7423950fa2a6142bb806567b77dcb9b01a
Rancor38 <zak.rancor@hotmail.com>
2022-08-26 11:55:16 -0700

    Added framer motion for buttons

--- src/components/notesComponents/AddNoteButton.js
+++ src/components/notesComponents/AddNoteButton.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 const AddNoteButton = () => {
     return (
         <h1>
-            <Button variant="dark" href='/new-note'>Add Note</Button>{' '}
+            <Link to='/new-note'>
+                <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    className='button'
+                    onClick={() => null}
+                >
+                    Add Note
+                </motion.button>{' '}
+            </Link>
         </h1>
     );
 };
